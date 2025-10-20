3c83b53bbcb6b240e01a6cf834b234c106391666
PauloS5 <paulosocratesdesouza@gmail.com>
2025-10-19 19:57:34 -0500

    Gerador de dailys criado

---
+++ sgrds/src/app/generate-daily/generate-daily.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-daily',
+  imports: [],
+  templateUrl: './generate-daily.component.html',
+  styleUrl: './generate-daily.component.css'
+})
+export class GenerateDailyComponent {
+
+}
