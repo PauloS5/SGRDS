e0e411f3e94ef70e2e9eefa056386a69230bd55a
PauloS5 <paulosocratesdesouza@gmail.com>
2025-10-19 19:57:53 -0500

    Rota criada

--- sgrds/src/app/app.routes.ts
+++ sgrds/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { GenerateDailyComponent } from './generate-daily/generate-daily.component';
 
 export const routes: Routes = [
-    {path: "", component: HomepageComponent}
+    {path: "", component: HomepageComponent},
+    {path: "daily/generate",  component: GenerateDailyComponent}
 ];
