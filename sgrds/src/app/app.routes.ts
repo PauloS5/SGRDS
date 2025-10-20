b1859c51536d91a78e687678eaaeaf1bb824f946
PauloS5 <paulosocratesdesouza@gmail.com>
2025-10-19 22:09:16 -0500

    Lista de Dailys criada

--- sgrds/src/app/app.routes.ts
+++ sgrds/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DailyFormComponent } from './daily/daily-form/daily-form.component';
+import { DailyListComponent } from './daily/daily-list/daily-list.component';
 
 export const routes: Routes = [
     {path: "", component: HomepageComponent},
-    {path: "daily/new",  component: DailyFormComponent}
+    {path: "daily/new",  component: DailyFormComponent},
+    {path: "daily/list",  component: DailyListComponent}
 ];
---
+++ sgrds/src/app/daily/daily-list/daily-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-list',
+  imports: [],
+  templateUrl: './daily-list.component.html',
+  styles: ``
+})
+export class DailyListComponent {
+
+}
