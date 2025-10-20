f483bbddee472d4ad72df637d4bda20f9022265b
PauloS5 <paulosocratesdesouza@gmail.com>
2025-10-19 22:35:43 -0500

    Novo componente adicionado

--- sgrds/src/app/app.routes.ts
+++ sgrds/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DailyFormComponent } from './daily/daily-form/daily-form.component';
 import { DailyListComponent } from './daily/daily-list/daily-list.component';
+import { DailyComponent } from './daily/daily.component';
 
 export const routes: Routes = [
     {path: "", component: HomepageComponent},
+    {path: "daily", component: DailyComponent},
     {path: "daily/new",  component: DailyFormComponent},
     {path: "daily/list",  component: DailyListComponent}
 ];
